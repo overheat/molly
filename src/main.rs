5f4b2f3804e2404f9e5dd565633a49db432a6408
Aaron Tsui <aaron.tsui@outlook.com>
2024-02-28 11:48:25 +0800

    atomics and locks, but u8

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use anyhow::{Context, Result};
 use clap::Parser;
 use log::{info, warn};
+use std::sync::atomic::{AtomicU8, Ordering};
 
-static LOG_LEVEL: u8 = 0;
+static LOG_LEVEL: AtomicU8 = AtomicU8::new(0);
+
+pub fn get_log_level() -> u8 {
+    LOG_LEVEL.load(Ordering::Relaxed)
+}
+
+pub fn set_log_level(level: u8) {
+    LOG_LEVEL.store(level, Ordering::Relaxed);
+}
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -19,7 +28,9 @@ fn main() -> Result<()> {
     let args = Cli::parse();
     info!("starting up");
     warn!("oops, nothing implemented!");
-    info!("global value is {LOG_LEVEL}");
+    set_log_level(1);
+    let a = get_log_level();
+    info!("global value is {a}");
 
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;

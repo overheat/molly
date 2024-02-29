1ee31a87ddf2027da6fcd262507249b394dcfd8f
Aaron Tsui <aaron.tsui@outlook.com>
2024-02-29 11:38:37 +0800

    add mqtt client without alpn for now

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,18 @@ pub mod config;
 pub use config::Configs;
 
 pub const CONFIG_FILE: &str = "configs/config.toml";
+
+pub const HELLO_WORLD_TOPIC: &str = "hello/world";
+pub const AWS_IOT_MQTT_ALPN: &str = "mqtt";
+// both Signature Version 4	and Custom authentication.
+pub const MQTT_OVER_WSS_PORT: u16 = 443;
+// X.509 client certificate
+pub const MQTT_WITH_ALPN_PORT: u16 = 443;
+pub const MQTT_OVER_TLS_PORT: u16 = 8883;
+// Custom authentication
+pub const MQTT_CUSTOM_AUTH_PORT: u16 = 443;
+// MQTT without TLS, not support by AWS IoT core.
+pub const MQTT_PORT: u16 = 8883;
+// MQTT keepalive interval support by AWS IoT core.
+// Minimal is 30 secondes.
+pub const KEEP_ALIVE_INTERVAL: u64 = 30;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use anyhow::{Context, Result};
 use clap::Parser;
 use log::{info, warn};
+use molly::{AWS_IOT_MQTT_ALPN, HELLO_WORLD_TOPIC, KEEP_ALIVE_INTERVAL};
+use rumqttc::{AsyncClient, MqttOptions, QoS, TlsConfiguration, Transport};
+use std::fs;
+use std::{error::Error, time::Duration};
 
+
+const CLIENT_ID: &str = "molly";
 
 // Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -12,22 +18,71 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<()> {
+// fn main() -> Result<()> {
+#[tokio::main(flavor = "current_thread")]
+
+async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let args = Cli::parse();
-    info!("starting up");
-    warn!("oops, nothing implemented!");
-
     molly::Configs::init();
-    print!("{:?}", molly::Configs::global());
+    // print!("{:?}", molly::Configs::global());
 
-    let content = std::fs::read_to_string(&args.path)
-        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+    let name = CLIENT_ID;
+    let endpoint = molly::Configs::global().iot_ats.to_string();
+    // let ca = molly::Configs::global().ca.to_string();
+    // let cert = molly::Configs::global().cert.to_string();
+    // let key = molly::Configs::global().key.to_string();
 
-    molly::find_matches(&content, &args.pattern, &mut std::io::stdout());
+    info!("{name} is connecting to {endpoint}");
+    let mut mqttoptions = MqttOptions::new(name, endpoint, 8883);
+    mqttoptions.set_keep_alive(std::time::Duration::from_secs(KEEP_ALIVE_INTERVAL));
+
+    let ca = include_bytes!("../certs/AmazonRootCA1.pem");
+    let client_cert = include_bytes!("../certs/certificate.pem.crt");
+    let client_key = include_bytes!("../certs/private.pem.key");
+
+    let ca = ca.to_vec();
+
+    let transport = Transport::Tls(TlsConfiguration::Simple {
+        ca,
+        alpn: None,
+        client_auth: Some((client_cert.to_vec(), client_key.to_vec())),
+    });
+
+    mqttoptions.set_transport(transport);
+
+    let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+
+    client
+    .subscribe(HELLO_WORLD_TOPIC, QoS::AtMostOnce)
+    .await
+    .unwrap();
+
+    loop {
+        let notification = match eventloop.poll().await {
+            Ok(v) => {
+                println!("Event = {v:?}");
+            }
+            Err(e) => {
+                println!("Error = {e:?}");
+                break;
+            }
+        };
+        println!("Received = {:?}", notification);
+    }
 
     Ok(())
+
+    // let args = Cli::parse();
+    // warn!("oops, nothing implemented!");
+
+
+    // let content = std::fs::read_to_string(&args.path)
+    //     .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    // molly::find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+    // Ok(())
 }
 
 
